17a663fd77330a49189b05425ad08f198038ad43
dreadlord-sedai <dahamifabbio@gmail.com>
2025-08-17 19:33:42 +0530

    webpack-practice

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,4 +1,6 @@
+// webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,4 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
