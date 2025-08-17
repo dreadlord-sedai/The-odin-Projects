50046fc52cb5177f56efbe47b94684c7e65dff1d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-08-17 19:55:13 +0530

    webpack-practice

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -2,3 +2,10 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+import odinImage from "./odinImage.jpeg";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
