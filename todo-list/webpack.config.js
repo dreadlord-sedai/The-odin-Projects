8c229e1a837b1c4a837303250aeec66e4fdab2b0
dreadlord-sedai <dahamifabbio@gmail.com>
2025-08-19 14:28:07 +0530

    Code todolist functionality

--- todo-list/webpack.config.js
+++ todo-list/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,4 +20,12 @@ module.exports = {
     ],
   },
   mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
