d11a5262f5e72eadb7177b96b338a581efb3b7d7
dreadlord-sedai <dahamifabbio@gmail.com>
2025-08-19 14:40:35 +0530

    Code todolist functionality

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,4 +1,14 @@
 import App from './App';
+import './styles.css';
+import Todo from './Todo.js';
+import UI from './UI.js';
+
+const todo = new Todo();
+todo.render();
+
+
+const ui = new UI();
+ui.renderTodos();
 
 const app = new App();
 app.render();
--- todo-list/webpack.config.js
+++ todo-list/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/todo-list/dist/',
     clean: true,
   },
   devServer: {
